7194b89c0b4163e078906cfe7abbffc275b61599
Flix <Flix01@users.noreply.github.com>
2016-12-12 17:30:49 +0100

    addons/imguinodegrapheditor/imguinodegrapheditor.cpp: Minor changes to the demo code to make old saved files still usable.

--- addons/imguinodegrapheditor/imguinodegrapheditor.cpp
+++ addons/imguinodegrapheditor/imguinodegrapheditor.cpp
@@ -2494,10 +2494,10 @@ enum MyNodeTypes {
     MNT_COMBINE_NODE,
     MNT_COMMENT_NODE,
     MNT_COMPLEX_NODE,
-    MNT_OUTPUT_NODE,
 #   ifdef IMGUI_USE_AUTO_BINDING
     MNT_TEXTURE_NODE,
 #   endif
+    MNT_OUTPUT_NODE = 5,    // One problem here when adding new values is backward compatibility with old saved files: they rely on the previously used int values (it should be OK only if we append new values at the end).
     MNT_COUNT
 };
 // used in the "add Node" menu (and optionally as node title names)
